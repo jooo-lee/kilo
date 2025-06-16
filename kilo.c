1f1fe1e4d1b54b58b8c786f9ba7235bc5f81ad87
Joseph Lee <josephlee3004@gmail.com>
2025-06-16 19:33:17 -0400

    Display keypresses

--- kilo.c
+++ kilo.c
@@ -1,5 +1,7 @@
 // Tutorial from https://viewsourcecode.org/snaptoken/kilo/
 
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -21,31 +23,41 @@ void enableRawMode() {
 	// Read terminal attributes into termios struct
 	struct termios raw = orig_termios;
 
-	/* 
-	c_lflag is for "local flags" or "miscellaneous flags",
-	one of which is the bitflag ECHO. Here we are flipping bits
-	to turn off the ECHO feature and turn off canonical mode.
-	Turning off canonical mode means we are reading input byte-by-byte
-	instead of line-by-line. Now the program will quit as soon as 'q'
-	is pressed.
-	*/ 
+	/* c_lflag is for "local flags" or "miscellaneous flags",
+	   one of which is the bitflag ECHO. Here we are flipping bits
+	   to turn off the ECHO feature and turn off canonical mode.
+	   Turning off canonical mode means we are reading input byte-by-byte
+	   instead of line-by-line. Now the program will quit as soon as 'q'
+	   is pressed. */ 
 	raw.c_lflag &= ~(ECHO | ICANON);
 
-	/* 
-	Apply new terminal attributes to terminal.
-	TCSAFLUSH specifies when to apply the change.
-	In this case, it waits for all pending output to be written 
-	to the terminal, and discards any input that hasn't been read.
-	*/
+	/* Apply new terminal attributes to terminal.
+	   TCSAFLUSH specifies when to apply the change.
+	   In this case, it waits for all pending output to be written 
+	   to the terminal, and discards any input that hasn't been read. */
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main() {
 	enableRawMode();
-	
+
 	char c;
 
 	// Keep reading until end of file or 'q' is pressed
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+		/* Test whether a character is a control character. 
+		   Control characters are nonprintable characters that we don't
+		   want to print to the screen, e.g. ESC or TAB. */
+		if (iscntrl(c)) {
+			/* %d tells printf() to format the byte as a decimal 
+			number (its ASCII code). */
+			printf("%d\n", c);
+		} else {
+			/* %c tells printf() to write out the byte directly,
+			   as a character. */
+			printf("%d ('%c')\n", c, c);
+		}
+	}
+
 	return 0;
 }
