0de795f13b6b8ec4f7209b3dbbcf810266636ba5
Joseph Lee <josephlee3004@gmail.com>
2025-06-25 14:26:48 -0400

    Clear screen and reposition cursor on exit

--- kilo.c
+++ kilo.c
@@ -22,8 +22,12 @@ struct termios orig_termios;
 
 /*** terminal ***/
 
-// Print error message and exit program with status of 1
 void die(const char *s) {
+	// Clear screen and reposition cursor on exit
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+
+	// Print error message and exit program with status of 1
 	perror(s);
 	exit(1);
 }
@@ -86,6 +90,10 @@ void editorProcessKeypress() {
 
 	switch (c) {
 		case CTRL_KEY('q'):
+			// Clear screen and reposition cursor on exit
+			write(STDOUT_FILENO, "\x1b[2J", 4);
+			write(STDOUT_FILENO, "\x1b[H", 3);
+
 			exit(0);
 			break;
 	}
